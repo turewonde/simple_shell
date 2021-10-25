156f3a6f55e4b55a8628f71dd01778075630b009
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-10-25 05:36:21 -0700

    Created program.c file

--- shell.h
+++ shell.h
@@ -7,4 +7,23 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 #include <sys/stat.h>
-/* incomplete this will be edit over time */
+#define BUFFER_LEN 1024
+extern char **environ;
+
+char *concat_all(char *s1, char *s2);
+char *find_path(char *av);
+char **split_line(char *line, int bufsize);
+int check_builtin(char *argvv);
+void print_env(void);
+void builtins(char **argvv, int i);
+void exec_command(char **argvv);
+void __exit(char **argvv);
+void change_dir(char **argvv);
+int _strcmp(char *s1, char *s2);
+int _atoi(char *s);
+int _strlen(char *s);
+char *read_line();
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_getenv(char *env);
+
+#endif /*_HEADER_*/
